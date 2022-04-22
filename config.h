fb4060926bbebc6cce905593407132844a25d89d
voldari <isaacharper@gmail.com>
2022-04-21 20:17:20 -0700

    fix layer ordering

--- config.h
+++ config.h
@@ -8,6 +8,8 @@
 #    define RGBLIGHT_LIMIT_VAL 150
 #endif
 
+#define DEFAULT_LAYER = 1
+
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 //#define SPLIT_USB_DETECT
 //#define NO_USB_STARTUP_CHECK
--- keymap.c
+++ keymap.c
@@ -5,42 +5,65 @@
 char wpm_str[10];
 
 enum layers {
-    _QWERTY = 0,
+    _BASE,
+    _QWERTY = 1,
     _NAV,
     _SYM,
-    _FUNCTION,
 };
 
 
 // Aliases for readability
-#define QWERTY   DF(_QWERTY)
+#define BASE   DF(_BASE)
 
 #define HOME     MO(_QWERTY)
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
-#define FKEYS    MO(_FUNCTION)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+
+/*
+ * Base Layer: Base
+ *
+ * ,-------------------------------------------.                              ,-------------------------------------------.
+ * |        |      |      |      |      |      |                              |      |      |      |      |      |  Bksp  |
+ * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |                              |      |      |      |      |      |  tab   |
+ * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      | del    |
+ * `----------------------+------+------+------+Space +------|  |------+enter +------+------+------+----------------------'
+ *                        |      |Lshif | ctrl |      |Esc   |  | Sym  |      |  alt |  nav |      |
+ *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        `----------------------------------'  `----------------------------------'
+ */
+    [_BASE] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, KC_BSPC,
+    _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, KC_TAB ,
+    _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, XXXXXXX, _______, _______, _______, _______, _______, KC_DEL ,
+                               _______, KC_LSFT, KC_LCTL, KC_SPC , KC_ESC , SYM    , KC_ENT , KC_LALT, NAV    , _______
+    ),
+
+
 /*
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |Ctrl/Esc|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+ * |        |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  tab   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | '    " |
+ * |  "  '  |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  tab   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |  alt   |   Z  |   X  |   C  |   V  |   B  | Juno |      |  | func |      |   N  |   M  | ,  < | . >  | /  ? | del    |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |enter | Space| Lshif| ctrl |  | Sym  | Nav  | Space|enter |      |
+ * |        |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? | del    |
+ * `----------------------+------+------+------+Space +------|  |------+enter +------+------+------+----------------------'
+ *                        |      |Lshif | ctrl |      |Esc   |  | Sym  |      |  alt |  nav |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
-     KC_TAB  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOTE,
-     KC_LALT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LGUI, XXXXXXX, FKEYS  , XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_DEL ,
-                                _______, KC_ENT , KC_SPC , KC_LSFT, KC_LCTL, SYM    , NAV    , KC_SPC , KC_ENT , _______
+    _______, KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
+   KC_QUOTE, KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, _______,
+    _______, KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX, _______, _______, XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
+                              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 
@@ -48,65 +71,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Nav Layer: Media, navigation
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |                              |      |      |      |      |      |  bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        | Left | Down |  Up  |Right |      |                              |      |      |      |      |      |        |
+ * |        | Left | Down |  Up  |Right |      |                              |      | Left | Down |  Up  |Right |  tab   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  | PASS | PASS | PASS |      |      |
+ * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |  del   |
+ * `----------------------+------+------+------+ space+------|  |------+ enter+------+------+------+----------------------'
+ *                        |      |Lshif | ctrl |      |Esc   |  | Sym  |      |  alt |  nav |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,                                     XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
+
 
 
 /*
   * Sym Layer: Numbers and symbols
   *
   * ,-------------------------------------------.                              ,-------------------------------------------.
-  * |        |      |      |CLock |   (  |   )  |                              |   `  |   1  |   2  |   3  |  0   |  Bksp  |
+  * |    F9  | F10  | F11  | F12  |   (  |   )  |                              |   `  |   1  |   2  |   3  |  0   |  Bksp  |
   * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
-  * |        |      |      |NLock |   [  |   ]  |                              |   -  |   4  |   5  |   6  |  \   |        |
+  * |    F5  |  F6  |  F7  |  F8  |   [  |   ]  |                              |   -  |   4  |   5  |   6  |  \   |  tab   |
   * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-  * |        |      |      |SLock |   <  |   >  |      |      |  |      |      |   =  |   7  |   8  |   9  |      |   Ddel  |
-  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-  *                        |      |      |      |Shift |      |  |      |      |      |      |      |
+  * |    F1  |  F2  |  F3  |  F4  |   <  |   >  |      |numloc|  |caploc|      |   =  |   7  |   8  |   9  |      |   Del  |
+  * `----------------------+------+------+------+ space+------|  |------+ enter+------+------+------+----------------------'
+  *                        |      |Lshif | ctrl |      |Esc   |  | Sym  |      |  alt |  nav |      |
   *                        |      |      |      |      |      |  |      |      |      |      |      |
   *                        `----------------------------------'  `----------------------------------'
   */
     [_SYM] = LAYOUT(
-     XXXXXXX , XXXXXXX, XXXXXXX, KC_CAPS, KC_LPRN, KC_RPRN,                                     KC_GRV ,  KC_7  ,  KC_8  ,   KC_9  ,   KC_0 , KC_BSPC,
-     XXXXXXX , XXXXXXX, XXXXXXX, KC_NLCK, KC_LBRC, KC_RBRC,                                     KC_MINS,  KC_4  ,  KC_5  ,   KC_6  , KC_BSLS, XXXXXXX,
-     XXXXXXX , XXXXXXX, XXXXXXX, KC_SLCK, KC_LT  , KC_GT  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQL ,  KC_1  ,  KC_2  ,   KC_3  , XXXXXXX, KC_DEL,
-                                 XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-    ),
-
-
-/*
- * Function Layer: Function keys
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |  F9  | F10  | F11  | F12  |      |        |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              |  F5  |  F6  |  F7  |  F8  |      |        |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |  F1  |  F2  |  F3  |  F4  |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
-    [_FUNCTION] = LAYOUT(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                      KC_F9 ,  KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                      KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  XXXXXXX, XXXXXXX,
-                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+       KC_F9 ,  KC_F10,  KC_F11,  KC_F12, KC_LPRN, KC_RPRN,                                     KC_GRV ,  KC_7  ,  KC_8  ,   KC_9  ,   KC_0 , _______,
+       KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , KC_LBRC, KC_RBRC,                                     KC_MINS,  KC_4  ,  KC_5  ,   KC_6  , KC_BSLS, _______,
+       KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_LT  , KC_GT  , XXXXXXX, KC_NLCK, KC_CAPS, XXXXXXX, KC_EQL ,  KC_1  ,  KC_2  ,   KC_3  , XXXXXXX, _______,
+                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 /*
@@ -133,9 +135,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-
 #ifdef OLED_ENABLE
     oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
+
+
 
 
     #define TAP_FRAMES 2
@@ -294,9 +297,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             case _SYM:
                 oled_write_P(PSTR("Sym\n"), false);
                 break;
-            case _FUNCTION:
-                oled_write_P(PSTR("Function\n"), false);
-                break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
         }
@@ -319,6 +319,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         if (!is_keyboard_master()) {
             render_layer();
             render_status();
+             
         }
 
         return false;
@@ -349,4 +350,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void suspend_power_down_user(void) {
     oled_off();
+}
+
+void suspend_wakeup_init_user(void) {
+    set_single_persistent_default_layer(_QWERTY);
 }
